e5ccdcef7e823a9dca05acc34c8cba02e7df6e2e
carlos-ventura <carlospinho_pt@hotmail.com>
2024-03-09 02:57:51 +0100

    Refactor Translator

--- app/translator.py
+++ app/translator.py
@@ -1,27 +1,28 @@
 import re
 
+
 class Translator():
-    def __init__(self, translated_data: str, path: str = None):
+    def __init__(self, translated_data: dict, path: str):
         self._translated_data = translated_data
         self._path = path
 
-    def read_subtitles(self):
-        with open(self._path, 'r', encoding="utf-8") as handle:
-            return handle.read()
-
-    def write_subtitles(self, path: str, content: str):
-        with open(path, 'w', encoding="utf-8") as handle:
-            handle.write(content)
-
-    def translate_pokemon_names(self, content: str, translated_pokemons: dict):
-        for key, value in translated_pokemons.items():
-            content = re.sub(pattern=r'\b' + key + r'\b', repl=value, string=content, flags=re.IGNORECASE)
-        return content
-    
     def run(self, path: str = None):
         if path:
             self._path = path
-        old_subtitles = self.read_subtitles()
-        new_subtitles = self.translate_pokemon_names(old_subtitles, self._translated_data)
-        self.write_subtitles(self._path, new_subtitles)
-    
+
+        old_subtitles = self._read_subtitles()
+        new_subtitles = self._translate(old_subtitles)
+        self._write_subtitles(self._path, new_subtitles)
+
+    def _read_subtitles(self):
+        with open(self._path, 'r', encoding="utf-8") as input_file:
+            return input_file.read()
+
+    def _translate(self, content: str):
+        for key, value in self._translated_data.items():
+            content = re.sub(pattern=r'\b' + key + r'\b', repl=value, string=content, flags=re.IGNORECASE)
+        return content
+
+    def _write_subtitles(self, path: str, content: str):
+        with open(path, 'w', encoding="utf-8") as output_file:
+            output_file.write(content)
