b23d9ca3af25a2f8644d64271eb0e24d420d8adf
carlos-ventura <carlospinho_pt@hotmail.com>
2024-03-09 02:25:51 +0100

    Extend constants

--- app/constants.py
+++ app/constants.py
@@ -1,2 +1,9 @@
-TRANSLATION_DATA = "translated-data"
+TRANSLATION_FILENAME = "translation_mapping"
 DEFAULT_TRACK_NUMBER = 2
+
+DESCRIPTION = "Returns subtitle file with translated Pokemons and more"
+PATH_HELP = "Full path of the subtitle target file or folder"
+ADD_HELP = "Adds new entry to the translated json. 'Japonese_name:English_name'. If multiple separate by |"
+GENERATE_SUBS_HELP = "Folder with mkv files. Generates all sub files for a folder outside called subs"
+TRACK_HELP = "Track number to get subs"
+UPDATE_HELP = "Update the translation list from Bulbapedia"
