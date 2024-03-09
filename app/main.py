a956f8a153ec5a3b638bac8ffa2d38f6a4081ee5
carlos-ventura <carlospinho_pt@hotmail.com>
2024-03-09 03:52:06 +0100

    Clean and refactor main

--- app/main.py
+++ app/main.py
@@ -1,57 +1,11 @@
 import argparse
-import glob
-import mimetypes
 import os
-import subprocess
 from pathlib import Path
 
+from action_handlers import convert_str_to_dict, generate_subs, translate_subs
 from constants import *
 from translations import Translations
 from translator import Translator
-
-
-def convert_str_to_dict(entries: str):
-    all_dicts = {}
-    for entry in entries.split('|'):
-        key, value = entry.split(':')
-        all_dicts[key.strip()] = value.strip()
-    return all_dicts
-
-
-def generate_subs(file: str, sub_file: str, track_number: int):
-    command_string = f'mkvextract "{file}" tracks {track_number}:"{sub_file}"'
-    subprocess.Popen(command_string, stdout=subprocess.PIPE, shell=True)
-
-
-def translate_subs(translator: Translator, videos_folder: str):
-    subs_filenames = get_filenames(videos_folder, video=False)
-    for file in subs_filenames:
-        print(f"[+] {file}")
-        translator.run(path=file)
-
-
-def get_filenames(videos_folder: str, video: bool = True):
-    filenames = glob.glob(os.path.join(videos_folder, "*.*"))
-    video_filenames = []
-    sub_filenames = []
-    for file in filenames:
-        mime = mimetypes.guess_type(file)
-        if mime[0] is None:
-            continue
-        if "video" in mime[0]:
-            video_filenames.append(file)
-        else:
-            sub_filenames.append(file)
-    return video_filenames if video else sub_filenames
-
-
-def read_files(videos_folder: str, track_number: int):
-    video_filenames = get_filenames(videos_folder)
-    for file in video_filenames:
-        file_list = file.split(".")
-        file_list[-1] = "ass"
-        sub_file = ".".join(file_list)
-        generate_subs(file, sub_file, track_number)
 
 
 def parse_arguments():
@@ -67,23 +21,37 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
+
     track_number = args.track or DEFAULT_TRACK_NUMBER
     print(f"[+] Track number is {track_number}")
+
     translations = Translations()
+
     path_exists = Path(f'data/{TRANSLATION_FILENAME}.json').exists()
+
     if not path_exists:
+        print("[+] Creating translations...")
         translations.upsert(update=False)
-        print("[+] Create name list")
+
     if args.update:
+        print("[+] Updating translations...")
         translations.upsert(update=True)
-        print("[+] Update name list from the web")
-    if add := args.add:
-        add_dicts = convert_str_to_dict(add)
-        translations.add_entries(add_dicts)
-        print(f"[+] Entry {add} added to the name list")
+
+    if entries := args.entries:
+        entry_dicts = convert_str_to_dict(entries)
+        for key, value in entry_dicts:
+            print(f"[+] Adding entry {key}:{value} translations...")
+        translations.add_entries(entry_dicts)
+
+    if videos_folder := args.generate_subs:
+        print("[+] Extracting subtitles from the video files...")
+        generate_subs(videos_folder, track_number)
+
     if path := args.path:
+        print("Translating...")
         if not args.update and path_exists:
             translations.load()
+
         translated_data = translations.translated_data
         translator = Translator(translated_data, path)
         if os.path.isdir(path):
@@ -93,6 +61,3 @@ if __name__ == '__main__':
             print("[+] Single file input")
             translator.run()
             print(path)
-    if videos_folder := args.generate_subs:
-        print("[+] Extracting subtitles from the video files...")
-        read_files(videos_folder, track_number)
